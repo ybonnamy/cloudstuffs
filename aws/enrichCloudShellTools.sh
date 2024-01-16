a386a89eea5ee6d83d6100dc019d289bdf90b6ae
ybonnamy <yann_bonnamy@yahoo.fr>
2024-01-16 09:01:07 +0000

    aws - enrichCloudShellTools for gcloud

--- aws/enrichCloudShellTools.sh
+++ aws/enrichCloudShellTools.sh
@@ -2,4 +2,16 @@
 sudo dnf -y install bind-utils virt-what amazon-ec2-net-utils ansible yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
-
+sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el9-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+sudo dnf install -y google-cloud-cli
+pip install google-auth
+echo gcloud auth login
+echo gcloud auth application-default login
