55e0f257ea73947d20b5b5d669b889f78a5cfe51
ybonnamy <yann_bonnamy@yahoo.fr>
2024-01-28 18:23:38 +0000

    better way to add ansible-galaxy collection amazon.aws

--- gcp/enrichCloudShellTools.sh
+++ gcp/enrichCloudShellTools.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo apt install -y awscli ansible python3-boto3
 pip install requests "urllib3<2"  "Jinja2<3.1"
-ansible-galaxy collection install amazon.aws
-
+ansible-galaxy install -r ansible/requirements.yml
